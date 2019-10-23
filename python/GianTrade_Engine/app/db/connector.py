45fc7edf36b443edc9a503409ac3d0e39358f2ce
Vahegian <vahegian@gmail.com>
2019-10-24 00:09:37 +0200

    added 'update' and 'delete' methods to 'connector'

--- python/GianTrade_Engine/app/db/connector.py
+++ python/GianTrade_Engine/app/db/connector.py
@@ -10,25 +10,19 @@ class Connector:
         self._engine = p2.connect(database = db, user = user, password = password, host = host, port = port)
         self._cur = self._engine.cursor()
 
-    # def create_db(self, dbName):
-    #     if self._engine != None:
-    #         self._engine.execute(f"create database {dbName}")
-
     def create_table(self, tname, cols):
-        self._cur.execute(f'''CREATE TABLE {tname}
+        self.execute(f'''CREATE TABLE {tname}
                     ({cols});''')
-        self._engine.commit()
 
     def drop_table(self, tname):
-        self._cur.execute(f'''DROP TABLE {tname};''')
-        self._engine.commit()
+        self.execute(f'''DROP TABLE {tname};''')
+        
 
     def insert_into_table(self, tname, cols, rows):
-        self._cur.execute(f'''INSERT INTO {tname}
+        self.execute(f'''INSERT INTO {tname}
                     ({cols})
                     VALUES
                     {rows};''')
-        self._engine.commit()
 
     def select_from(self, tnames, cols, conds=None, order_col=None, dec=False, limit=None):
         options = [f"SELECT {cols}", f"FROM {tnames}", f"WHERE {conds}", f"ORDER BY {order_col}", "DESC", f"LIMIT {limit}"]
@@ -52,9 +46,46 @@ class Connector:
             qString+=" "+options[index]
         qString+=";"
         print(qString)
+        if self._cur!=None:
+            self._cur.execute(qString)
+            return self._cur.fetchall()
+        else:
+            print("Please initialize db, use 'Connector.connect()' !!!")
+            return
+    
+    def update_table(self, tname, col, conds=None):
+        options = [f"UPDATE {tname}", f"SET {col}" ,f"WHERE {conds}"]
+        qString = ""
+
+        if tname==None or col==None:
+            return
+        else:
+            qString+=options[0]+" "+options[1]
+
+        if conds != None:
+            qString+=" "+options[2]+";"
+        else:
+            qString+=";"
+        self.execute(qString)
+
+    def delete_from(self, tname, conds):
+        options = [f"DELETE ", f"FROM {tname}" ,f"WHERE {conds}"]
+        qString = ""
+
+        if tname==None or conds==None:
+            return
+        else:
+            qString+=options[0]+" "+options[1]+" "+options[2]+";"
+        self.execute(qString)
         
-        self._cur.execute(qString)
-        return self._cur.fetchall()
+    
+    def execute(self, command):
+        if self._engine==None or self._cur==None:
+            print("Please initialize db, use 'Connector.connect()' !!!")
+            return
+        self._cur.execute(command)
+        self._engine.commit()
+        print(f"{command} OK.")
 
     def disconnect(self):
         self._engine.close()
@@ -68,7 +99,7 @@ if __name__ == "__main__":
     c = Connector()
     # c.connect('postgres://myuser@docker_db_1:5432/gtbinance')
     c.connect("gtbinance","myuser","pass")
-    c.drop_table("users")
+    # c.drop_table("users")
     c.create_table("users", '''id INT PRIMARY KEY NOT NULL,
                                 name TEXT NOT NULL,
                                 enc_pass CHAR(128), 
@@ -77,6 +108,11 @@ if __name__ == "__main__":
     c.insert_into_table("users", "id, name, enc_pass", '''(1, 'vahe', '9234708uj')''')
     c.insert_into_table("users", "id, name, enc_pass", '''(2, 'avahe', '9234708uj')''')
     print(c.select_from("users", "*", "enc_pass>'92'", "name", dec=True, limit=1))
+    c.update_table("users", "name='gang'", "id='1'")
+    print(c.select_from("users", "*"))
+    c.delete_from("users", "id='2'")
+    print(c.select_from("users", "*"))
+    c.drop_table("users")
     c.disconnect()
     # c.create_db("gian")
     # print(c.list_dbs())
