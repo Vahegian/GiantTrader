5c0aa6ba7d7e732cb43a0debe1b5373d7d4bdaed
Vahegian <vahegian@gmail.com>
2019-11-06 16:40:27 +0100

    add electron

--- electron/index.js
+++ electron/index.js
@@ -1,7 +1,38 @@
-const {app, BrowserWindow} = require('electron');
+//const exp = require('express');
+//const server = exp();
+const { app, BrowserWindow, Menu } = require("electron");
+
+
+// server.listen(9999, () => console.log("serving static files on port '9999'"))
+// server.use(exp.static('UI'))
+
+
+
+
+
+
+
+
+// Electron stuff
+
+Menu.setApplicationMenu(null);
+
 
 function boot(){
-	win = new BrowserWindow();
+	let mainWindow = new BrowserWindow({
+		width:1200,
+		height:800,
+		skipTaskbar: true,
+		toolbar: false
+	});
+	
+	mainWindow.loadURL('http://0.0.0.0:10001/index.html')
+	// Uncomment to use developer tools
+	mainWindow.webContents.openDevTools({detach:true});
+
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
 }
 
 app.on('ready', boot);
